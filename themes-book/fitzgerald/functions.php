6da6cc259d92d4b82a151c7e34df401a97d26ebc
Lukas <Kaiser>
2014-10-05 17:42:35 +0200

    Revert changes on Fitzgerald theme

--- themes-book/fitzgerald/functions.php
+++ themes-book/fitzgerald/functions.php
@@ -12,6 +12,3 @@ function fitzgerald_enqueue_styles() {
 	wp_enqueue_style( 'fitzgerald-fonts', '//fonts.googleapis.com/css?family=Crimson+Text:400,400italic,700|Roboto+Condensed:400,300,300italic,400italic' );
 }
 add_action( 'wp_print_styles', 'fitzgerald_enqueue_styles' );
-
-add_filter("pb_lists_add_numbers_to_list_elements", function(){return true;});
-add_filter("pb_lists_add_numbers_to_heading_levels", function(){return 1;});
